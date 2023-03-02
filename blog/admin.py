b31e4f1c073bdf2a75d9cd37be4df874d97a3254
2macs <adrian.mcneill@outlook.com>
2023-03-02 12:12:02 +0000

    fixed issue with static_root

--- blog/admin.py
+++ blog/admin.py
@@ -7,15 +7,15 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title')}
-    list_filter = ('status', 'created on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_filter = ('approved', 'created on')
+    list_filter = ('approved', 'created_on')
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
 
